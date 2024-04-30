fe43e2f2b852211a29a037642dfe946f7a0a83ae
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-30 18:55:49 +0600

    Add TanStack React Query Devtools and update page components to use React Query
    The changes include:
    
    * Adding `@tanstack/react-query-devtools` to `package.json` and `package-lock.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
 export default function Dashboard() {
-  return <div>This is Home page</div>;
+  const { data, isLoading, isError, isSuccess } = useQuery<any>({
+    queryKey: ["projects"],
+    queryFn: async () => {
+      try {
+        const response = await fetch("http://localhost:3001/projects");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return await response.json();
+      } catch (error: any) {
+        throw new Error("Error fetching data:", error);
+      }
+    },
+  });
+  console.log(data);
+  return <div> ok </div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 import { FieldType } from "@/types/formType";
-import { Button, Form, Input, Checkbox } from "antd";
+import { Button, Form, Input, Checkbox, Alert } from "antd";
 import type { FormProps } from "antd";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
-  
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  let email = "user@user.com";
+  let password = "123456";
+
   const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
-    console.log("Success:", values);
-    router.push("/dashboard");
+    if (values.email == email && values.password == password) {
+      setSuccess(true);
+      router.push("/dashboard");
+    } else {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 2000);
+    }
+    // console.log("Success:", values);
   };
 
   const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
@@ -17,50 +30,63 @@ export default function Home() {
   ) => {
     console.log("Failed:", errorInfo);
   };
+
+  let content = null;
+  if (success) {
+    content = <Alert message="login successful" type="success" showIcon />;
+  } else if (error) {
+    content = <Alert message="Wrong Credentials" type="warning" showIcon />;
+  } else {
+    content = null;
+  }
   return (
-    <div className="w-full h-[450px] flex justify-center items-center">
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item<FieldType>
-          label="Email"
-          name="email"
-          rules={[
-            { required: true, message: "Please input your email address!" },
-          ]}
+    <div className="w-full h-[450px]  justify-center items-center">
+      <div>{content}</div>
+      <div className="ml-[300px] pt-[100px]">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          className=""
         >
-          <Input />
-        </Form.Item>
+          <Form.Item<FieldType>
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: "Please input your email address!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item<FieldType>
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item<FieldType>
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 8, span: 16 }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+          <Form.Item<FieldType>
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 }
